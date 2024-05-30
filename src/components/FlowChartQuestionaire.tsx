f998d7dfeb25ae573d1b403dfb9ee6e2abc2f9cd
Shivit Kapoor <shivit2004@gmail.com>
2024-05-30 17:19:35 +0100

    Select multiple buttons in FlowChart

--- src/components/FlowChartQuestionaire.tsx
+++ src/components/FlowChartQuestionaire.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlowChartInterface } from './FlowChartInterface';
 import '../styles.css';
 
 const FlowChartQuestionaire: React.FC<FlowChartInterface> = ({ buttonsList, onNoneClick, onSubmitClick }) => {
+    const [selectedButtons, setSelectedButtons] = useState<string[]>([]);
+
+    const handleClick = (buttonData: string) => {
+        if (selectedButtons.includes(buttonData)) {
+            // If the button is already selected, remove it from the selectedButtons array
+            setSelectedButtons(selectedButtons.filter(btn => btn !== buttonData));
+        } else {
+            // If the button is not selected, add it to the selectedButtons array
+            setSelectedButtons([...selectedButtons, buttonData]);
+        }
+    };
+
     return (
         <div className='grid gird-row-4'>
             <div className='text-4xl row-span-1 pt-20'>Do you have any of these symptoms?</div>
             <div className='text-xl row-span-1 pt-10'>
                 {buttonsList.map((buttonData, index) =>
-                    <div className='flowChartButton p-4 m-6 rounded-full text-white  selection:bg-blue-500'>{buttonData}</div>
+                    <div className={`${selectedButtons.includes(buttonData) ? 'bgClickBlue' : 'bgBlue'} p-4 m-6 rounded-full text-white cursor-pointer`} onClick={() => handleClick(buttonData)}>{buttonData}</div>
                 )}
             </div>
-            <div className='row-span-1 flowChartNoneButton lg:mx-96 mx-40 py-4 mt-6 mb-4 rounded-full text-white text-2xl font-medium cursor-pointer'
+            <div className='row-span-1 flowChartNoneButton lg:mx-96 mx-32 md:mx-56 py-4 mt-6 mb-4 rounded-full text-white text-2xl font-medium cursor-pointer'
                 onClick={onNoneClick}>None</div>
-            <div className='row-span-1 flowChartSubmitButton lg:mx-96 mx-40 py-4 my-4 rounded-full text-white text-2xl font-medium cursor-pointer' onClick={onSubmitClick}>Submit</div>
+            <div className='row-span-1 flowChartSubmitButton lg:mx-96 mx-32 md:mx-56 py-4 my-4 rounded-full text-white text-2xl font-medium cursor-pointer' onClick={onSubmitClick}>Submit</div>
         </div>
     );
 };
