5cd225780ec7960e048cdc44f69aeeacc4cf0166
Shivit Kapoor <shivit2004@gmail.com>
2024-05-31 11:51:45 +0100

    Add navigate for homescreen

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -2,14 +2,27 @@
 
 import React from 'react';
 import homeScreenImage from "../components/HomeImage.jpeg"
+import {useNavigate} from 'react-router-dom';
 
 const HomeScreen: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClickPatient = () => {
+    // Pass arguments as an object
+    navigate('/PresentationForm');
+  };
+
+  const handleClickHospital = () => {
+    // Pass arguments as an object
+    navigate('/RegisterHospital');
+  };
+
   return (
     <div className='fixed inset-0 backgroundPale text-white flex flex-col items-center justify-center'>
       <div className='text-5xl textClickBlue font-bold max-w-md'>Welcome to quickER</div>
       <img src={homeScreenImage} alt="Image" className='h-96'/>
-      <div className='px-4'>Patient</div>
-      <div>Hospital</div>
+      <div className='px-4' onClick={handleClickPatient}>Patient</div>
+      <div onClick={handleClickHospital}>Hospital</div>
     </div>
   );
 };
