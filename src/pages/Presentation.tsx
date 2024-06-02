d79bfe94f2781918adc1ea78c0504630f719917c
kufupa <wilsonianprogrammer@gmail.com>
2024-06-02 16:46:52 +0100

    Fix: Text colour of input screen

--- src/pages/Presentation.tsx
+++ src/pages/Presentation.tsx
@@ -32,7 +32,7 @@ const PresentationForm: React.FC = () => {
   return (
     <div className="fixed inset-0 backgroundPale flex flex-col items-center justify-center">
       <form onSubmit={handleSubmit} className="w-full max-w-md p-8">
-        <h1 className="text-6xl font-bold mb-6 text-center textBlue">Describe your problem</h1>
+        <h1 className="text-5xl font-bold mb-6 text-center textClickBlue">Describe your problem</h1>
         <div className='relative'>
           <div className='relative left-20'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#3587b3" className="size-32">
@@ -51,7 +51,7 @@ const PresentationForm: React.FC = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
             </svg>
-            <div className="textBlue ml-2">Best to answer in 10-20 words</div>
+            <div className="textClickBlue font-bold ml-2 my-1">Best to answer in 10-20 words</div>
           </div>
 
           <textarea
