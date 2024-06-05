8af3335ef52975683cff7a004286ed0b3c7d6fe2
Shivit Kapoor <shivit2004@gmail.com>
2024-06-05 11:56:28 +0100

    UI for REDPATIENT

--- src/pages/RedPatients.tsx
+++ src/pages/RedPatients.tsx
@@ -1,14 +1,14 @@
 // @ts-nocheck
 
 import React from 'react';
-import ambulance from "../components/Ambulance.jpeg"
+import ambulance from "../components/AmbulanceImage.png"
 
 const RedPatients: React.FC = () => {
     return (
-        <div className="fixed inset-0 ambulaneBGColor text-white flex flex-col items-center justify-center">
-            <div className="text-5xl mb-20 max-w-2xl">You have been diagnosed as a Critical Patient</div>
-            <img src={ambulance} alt="Ambulance" className="mb-4" />
-            <div className="text-2xl border rounded-3xl max-w-80 p-10 mt-24">
+        <div className="backgroundPale items-center justify-center min-h-screen flex flex-col">
+            <div className="text-5xl mb-20 max-w-xl textClickBlue font-black tracking-wider">We think you are a Critical Patient</div>
+            <img src={ambulance} alt="Ambulance" className="mb-4 w-64" />
+            <div className="textBlue text-3xl border-4 border-blue-300 rounded-3xl max-w-lg p-10 mt-24 font-bold">
                 Please Call <span className="text-red-500">999</span> for additional assistance
             </div>
         </div>
