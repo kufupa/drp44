2ca203d7975d816fc64b734dcd83884521a69355
Shivit Kapoor <shivit2004@gmail.com>
2024-06-06 11:50:49 +0100

    New Hospital Screen(Ansh's version)

--- src/components/HospitalDiv.tsx
+++ src/components/HospitalDiv.tsx
@@ -4,6 +4,8 @@
 
 import React, { useState } from 'react';
 import { HospitalDetails } from './HospitalDetails';
+// @ts-ignore
+import LocationIcon from './LocationIcon.png'
 
 const HospitalDiv: React.FC<HospitalDetails> = ({ hospitalName, waitTime, distance, directions, ticked }) => {
     const [isTicked, setIsTicked] = useState(ticked);
@@ -20,41 +22,45 @@ const HospitalDiv: React.FC<HospitalDetails> = ({ hospitalName, waitTime, distan
 
     return (
         // Maybe have different views for different screens
-        <div className="min-h-24 relative p-4 border shadow-md m-4 bg-blue-100 hover:bg-blue-300 grid grid-cols-3 gap-4 rounded-full">
+        <div className="min-h-24 relative py-4 pr-20 md:pr-32 pl-4 border shadow-md m-4 bgBlue flex flex-col gap-4 rounded-3xl text-white">
             {/* Hospital Name */}
-            <div className="col-span-1 flex items-center justify-center">
-                <h2 className="text-2xl font-bold mb-2 max-w-96">{hospitalName}</h2>
+            <div className="">
+                <h2 className="text-2xl font-semibold max-w-fit">{hospitalName}</h2>
             </div>
 
             {/* Time */}
-            <div className="relative col-span-1 flex items-center justify-center text-lg">
-                <div className='absolute right-0'>
+            <div className="text-lg font-normal -mt-3 ml-0 flex pb-1">
+                <div className=''>
                     <div>
                         {waitTime < 1 ? `Wait Time: ${waitTime * 60} Mins` : `Wait Time: ${waitTime} Hours`}
                     </div>
-
-                    {distance < 1 ? `Distance: ${distance * 60} Mins` : `Distance: ${distance} Hours`}
+                    <div className='-mt-1.5'>
+                        {distance < 1 ? `Distance: ${distance * 60} Mins` : `Distance: ${distance} Hours`}
+                    </div>
                 </div>
             </div>
 
             {/* Direction and Inform Hospital */}
-            <div className="relative col-span-1 flex items-center justify-center">
-                <div className="absolute text-sm right-10 bottom-0 cursor-pointer hover:text-lg hover:-bottom-1"
-                    onClick={handleDirectionsClick}>Directions</div>
-                <div className="absolute cursor-pointer right-14 top-0" onClick={handleToggleTicked}>
-                    <div className="w-8 h-8 rounded-full bg-green-100 hover:bg-green-300">
+            <div className="absolute bottom-[10px] right-[16px] cursor-pointer" onClick={handleDirectionsClick}>
+                <img src={LocationIcon} alt="" className='w-10 rounded-full' />
+            </div>
+            {/* <div className="relative row-span-1 flex items-center justify-center"> */}
+            {/* <div className="absolute text-sm right-10 bottom-0 cursor-pointer hover:text-lg hover:-bottom-1"
+                    onClick={handleDirectionsClick}>Directions</div> */}
+            {/* <div className="absolute cursor-pointer top-0" onClick={handleToggleTicked}>
+                    <div className="w-8 h-8 rounded-full bg-white">
                         {isTicked ? (
                             // Render green tick if ticked is true
-                            <div className="w-8 h-8 rounded-full bg-green-100 hover:bg-green-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-green-600 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <div className="w-8 h-8 rounded-full bg-green-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2" fill="none" viewBox="0 0 24 24" stroke="textBlue"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                             </div>
                         ) : (
                             // Render plus sign if ticked is false
-                            <div className="absolute z-10 rounded-lg text-2xl left-2 -top-0.5">+</div>
+                            <div className="absolute z-10 rounded-lg text-4xl left-1.5 -top-1.5 textBlue font-extralight">+</div>
                         )}
                     </div>
-                </div>
-            </div>
+                </div> */}
+            {/* </div> */}
 
         </div>
     );
